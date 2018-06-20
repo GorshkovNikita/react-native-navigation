885ea7eae87612e418e2be12b5569807cfbbd065
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2018-06-21 00:07:07 +0300

    Added synchronized UI implementation

---
+++ android/app/src/main/java/com/reactnativenavigation/react/WixUIImplementationProvider.java
@@ -0,0 +1,44 @@
+package com.fitside.mobile;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.UIImplementation;
+import com.facebook.react.uimanager.UIImplementationProvider;
+import com.facebook.react.uimanager.UIManagerModule;
+import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.uimanager.events.EventDispatcher;
+
+import java.util.List;
+
+/**
+ * Created by Pavel on 06.06.2018.
+ */
+
+/**
+ * Provides UIImplementation to use in {@link UIManagerModule}.
+ */
+public class WixUIImplementationProvider extends UIImplementationProvider {
+    @Override
+    public UIImplementation createUIImplementation(
+            ReactApplicationContext reactContext,
+            UIManagerModule.ViewManagerResolver viewManagerResolver,
+            EventDispatcher eventDispatcher,
+            int minTimeLeftInFrameForNonBatchedOperationMs) {
+        return new CalendarUIImplementation(
+                reactContext,
+                viewManagerResolver,
+                eventDispatcher,
+                minTimeLeftInFrameForNonBatchedOperationMs);
+    }
+
+    @Override
+    public UIImplementation createUIImplementation(
+            ReactApplicationContext reactContext,
+            List<ViewManager> viewManagerList,
+            EventDispatcher eventDispatcher,
+            int minTimeLeftInFrameForNonBatchedOperationMs) {
+        return new CalendarUIImplementation(
+                reactContext,
+                viewManagerList,
+                eventDispatcher,
+                minTimeLeftInFrameForNonBatchedOperationMs);
+    }
+}
